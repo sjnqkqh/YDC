309b540ced76f17c41caa5b6e4359f1994dc6dca
JunLyul Yang <sjn3649@gmail.com>
2021-09-14 16:19:40 +0900

    Start small single project for studying Spring Data JPA, Spring Rest, Test Docs.

---
+++ src/main/java/com/ydc/boot/Ydc1Application.java
@@ -0,0 +1,13 @@
+package com.ydc.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ydc1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ydc1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ydc/boot/Ydc1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ydc.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ydc1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
